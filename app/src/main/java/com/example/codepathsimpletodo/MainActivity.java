34902c1f45f0a4653f34e6bbe567e5749b798f01
Dong Huang Chen <81045036+dong200k@users.noreply.github.com>
2022-01-11 18:04:05 -0500

    Initial Commit

--- app/src/main/java/com/example/codepathsimpletodo/MainActivity.java
+++ app/src/main/java/com/example/codepathsimpletodo/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportActionBar().setTitle("ToDo");
+        getSupportActionBar().setTitle("Simple To Do");
 
         // Mock data
         items = new ArrayList<>();
